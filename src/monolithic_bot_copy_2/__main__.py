65187a189a761ee9291b87b31e5d2ddb1b6c6e6f
MtkN1 <51289448+MtkN1@users.noreply.github.com>
2023-06-28 20:29:59 +0900

    Make plain

--- src/monolithic_bot_copy_2/__main__.py
+++
@@ -1,9 +0,0 @@
-import signal
-from contextlib import suppress
-
-from .main import main
-
-if __name__ == "__main__":
-    signal.signal(signal.SIGTERM, lambda *args: exit())
-    with suppress(KeyboardInterrupt):
-        main()
